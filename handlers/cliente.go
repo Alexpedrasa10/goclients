b722e3a8715482e2b54dfa2106d7481cc903e01a
Alex <alexpedrasa10@gmail.com>
2024-10-10 16:34:48 -0300

    add GET cliente id, PUT and DELETE routes

--- handlers/cliente.go
+++ handlers/cliente.go
@@ -6,6 +6,8 @@ import (
     "encoding/json"
     "log"
     "net/http"
+    "github.com/gorilla/mux"
+	"strconv"
 )
 
 func GetClients(w http.ResponseWriter, r *http.Request)  {
@@ -15,9 +17,7 @@ func GetClients(w http.ResponseWriter, r *http.Request)  {
 
     filas, err := database.Query("SELECT id, nombre, email, telefono FROM clientes")
 	
-	
 	if err != nil {
-		log.Fatalf("Error al obtener clientes: %v", err)
 		json.NewEncoder(w).Encode(models.APIResponse{
 			Code: http.StatusInternalServerError,
 			Data: "Error al procesar los datos del cliente",
@@ -40,6 +40,7 @@ func GetClients(w http.ResponseWriter, r *http.Request)  {
     })
 }
 
+
 func CreateClient(w http.ResponseWriter, r *http.Request)  {
 	
 	database := db.Connect()
@@ -52,10 +53,127 @@ func CreateClient(w http.ResponseWriter, r *http.Request)  {
     if err != nil {
         log.Fatalf("Error al preparar la inserción: %v", err)
     }
-    insertar.Exec(cliente.Nombre, cliente.Email, cliente.Telefono)
+
+    result, err := insertar.Exec(cliente.Nombre, cliente.Email, cliente.Telefono)
+    if err != nil {
+        json.NewEncoder(w).Encode(models.APIResponse{
+            Code: http.StatusInternalServerError,
+            Data: "Error al insertar cliente.",
+        })
+        return
+    }
+
+    lastInsertId, err := result.LastInsertId()
+    if err != nil {
+        json.NewEncoder(w).Encode(models.APIResponse{
+            Code: http.StatusInternalServerError,
+            Data: "Error al obtener el ID del cliente",
+        })
+        return
+    }
+
+    // Asignar el ID obtenido al cliente
+    cliente.ID = int(lastInsertId)
 
 	json.NewEncoder(w).Encode(models.APIResponse{
         Code: http.StatusOK,
         Data: cliente,
+    })
+}
+
+
+func UpdateCliente(w http.ResponseWriter, r *http.Request)  {
+
+    params := mux.Vars(r)
+    id, err := strconv.Atoi(params["id"])
+
+    if err != nil {
+		json.NewEncoder(w).Encode(models.APIResponse{
+			Code: http.StatusBadRequest,
+			Data: "ID Invalido",
+		})
+        return
+    }
+
+    database := db.Connect()
+	defer database.Close()
+
+    var cliente models.Cliente
+    json.NewDecoder(r.Body).Decode(&cliente)
+
+	_, err = database.Exec("UPDATE clientes SET nombre = ?, email = ?, Telefono = ? WHERE id = ?", cliente.Nombre, cliente.Email, cliente.Telefono, id)
+
+    json.NewEncoder(w).Encode(models.APIResponse{
+        Code: http.StatusOK,
+        Data: cliente,
+    })
+}
+
+func GetClient(w http.ResponseWriter, r *http.Request)  {
+
+    params := mux.Vars(r)
+    id, err := strconv.Atoi(params["id"])
+
+    if err != nil {
+		json.NewEncoder(w).Encode(models.APIResponse{
+			Code: http.StatusBadRequest,
+			Data: "ID Invalido",
+		})
+        return
+    }
+
+	database := db.Connect()
+	defer database.Close()
+
+    var cliente models.Cliente
+    
+    err = database.QueryRow(
+        "SELECT id, nombre, email, telefono FROM clientes WHERE id = ?", id).Scan(
+            &cliente.ID, &cliente.Nombre, &cliente.Email, &cliente.Telefono)
+
+    if err != nil {
+        
+        json.NewEncoder(w).Encode(models.APIResponse{
+            Code: http.StatusInternalServerError,
+            Data: "Error al obtener el cliente",
+        })
+        return
+    }
+
+	json.NewEncoder(w).Encode(models.APIResponse{
+        Code: http.StatusOK,
+        Data: cliente,
+    })
+}
+
+func DeleteClient(w http.ResponseWriter, r *http.Request)  {
+
+    params := mux.Vars(r)
+    id, err := strconv.Atoi(params["id"])
+
+    if err != nil {
+		json.NewEncoder(w).Encode(models.APIResponse{
+			Code: http.StatusBadRequest,
+			Data: "ID Invalido",
+		})
+        return
+    }
+
+    database := db.Connect()
+	defer database.Close()
+
+	_, err = database.Exec("DELETE from clientes WHERE id = ?", id)
+
+    if err != nil {
+        json.NewEncoder(w).Encode(models.APIResponse{
+            Code: http.StatusInternalServerError,
+            Data: "Error al borrar el usuario",
+        })
+        return
+    }
+
+    json.NewEncoder(w).Encode(models.APIResponse{
+        Code: http.StatusOK,
+        Data: "Usuario borrado con exito",
     })
 }
--- main.go
+++ main.go
@@ -14,6 +14,11 @@ func main() {
     // Rutas del CRUD
     r.HandleFunc("/clientes", handlers.GetClients).Methods("GET")
     r.HandleFunc("/clientes", handlers.CreateClient).Methods("POST")
+    
+    r.HandleFunc("/cliente/{id}", handlers.UpdateCliente).Methods("PUT")
+    r.HandleFunc("/cliente/{id}", handlers.GetClient).Methods("GET")
+    r.HandleFunc("/cliente/{id}", handlers.DeleteClient).Methods("DELETE")
+    
 
     log.Println("Servidor escuchando en http://localhost:8000")
     log.Fatal(http.ListenAndServe(":8000", r))
